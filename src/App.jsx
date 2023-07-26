33e3a1449a1bf9317ac41b0f5d03a2f3b6822a9f
yolovi <yolandalopevi@gmail.com>
2023-07-26 13:59:03 +0200

    started exercise greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Counter from './components/Counter/Counter'
+import Greeting from './components/Greeting/GreetingChild/GreetingChild'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
         <h2>Counter / Greeting</h2>
         <Counter initialValue = {0} number = {1}/>
         <Counter initialValue = {5} number = {5}/>
-
+        <Greeting/>
       </div>
     </>
   )
---
+++ src/components/Greeting/GreetingChild/GreetingChild.jsx
@@ -0,0 +1,6 @@
+const Greeting = () => {
+    return <h2>Hello, Euralio</h2>;
+
+}
+
+export default Greeting;
